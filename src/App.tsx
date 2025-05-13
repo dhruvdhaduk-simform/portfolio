a738c7e2ae98d583e946765a11383bc6f4c6731f
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-05-13 13:16:26 +0530

    feature: Create Header component with navlinks and style it

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Header } from '@/components/Header';
+
 function App() {
-    return <div>Hello, World</div>;
+    return (
+        <div className="w-full max-w-[100rem] m-auto">
+            <Header />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,57 @@
+export function Header() {
+    return (
+        <header className="fixed top-6 left-0 right-0 w-full flex justify-center">
+            <div className="w-full max-w-[100rem] px-9">
+                <div className="glassmorphic rounded-2xl h-14 flex justify-between">
+                    <a
+                        href="#"
+                        className="h-full p-2 pl-4 hover:scale-105 duration-200"
+                    >
+                        <img
+                            src="/logos/header_logo.png"
+                            alt=""
+                            className="h-full"
+                        />
+                    </a>
+
+                    <nav className="flex items-center gap-2 px-2">
+                        <NavLink href="#skills" className="hidden sm:inline">
+                            Skills
+                        </NavLink>
+                        <NavLink href="#projects" className="hidden sm:inline">
+                            Projects
+                        </NavLink>
+                        <NavLink href="#education" className="hidden sm:inline">
+                            Education
+                        </NavLink>
+                        <NavLink
+                            href="#contact-me"
+                            className="bg-white text-black hover:scale-105 duration-200"
+                        >
+                            Contact Me
+                        </NavLink>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+function NavLink({
+    href,
+    className,
+    children,
+}: {
+    href: string;
+    className?: string;
+    children: React.ReactNode;
+}) {
+    return (
+        <a
+            href={href}
+            className={`border border-transparent hover:border-[#ffffffc0] rounded-lg p-2 px-4 text-nowrap duration-300 ${className}`}
+        >
+            {children}
+        </a>
+    );
+}
