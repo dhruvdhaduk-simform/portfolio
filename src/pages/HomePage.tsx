c9021657a0b8bf9d4e16b0e29d5ec332d33c5376
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-05-13 15:29:43 +0530

    feature: Add external links in home page with border and background gradients

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,16 @@
+import { GRADIENTS } from '@/constants';
+
 export function HomePage() {
     return (
-        <div className="w-full h-full p-4 flex flex-col justify-center">
+        <div className="w-full h-full p-4 flex flex-col justify-center relative">
             <div className="flex flex-col gap-4 sm:gap-5 md:gap-6 lg:gap-7 xl:gap-8">
                 <p>Hi, I'm</p>
-                <p className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl bg-gradient-to-r from-[#9845E8] via-[#33D2FF] to-[#DD5789] inline-block text-transparent bg-clip-text">
+                <p
+                    className={`text-5xl sm:text-6xl md:text-7xl lg:text-8xl bg-gradient-to-r ${GRADIENTS[0].normal} inline-block text-transparent bg-clip-text`}
+                >
                     Dhruv Dhaduk
                 </p>
-                <p className="text-sm md:text-base">
+                <p className="text-sm md:text-base line-clamp-[20]">
                     I'm a frontend developer who enjoys building fast,
                     responsive web apps using React and modern JavaScript
                     frameworks. I focus on creating clean, interactive UIs with
@@ -20,6 +24,58 @@ export function HomePage() {
                     skills forward.
                 </p>
             </div>
+
+            <div className="absolute bottom-16 flex justify-start items-center flex-wrap gap-2 md:gap-4">
+                <ExternalLink
+                    href="mailto:dhruv.dhaduk@simformsolutions.com"
+                    imgURL="/logos/mail.svg"
+                    label="Send an email"
+                    borderGradient={GRADIENTS[1].normal}
+                    borderGradientHover={GRADIENTS[1].hover}
+                />
+                <ExternalLink
+                    href="https://in.linkedin.com/in/dhruvdhaduk"
+                    imgURL="/logos/linkedin.svg"
+                    label="LinkedIn"
+                    borderGradient={GRADIENTS[2].normal}
+                    borderGradientHover={GRADIENTS[2].hover}
+                />
+                <ExternalLink
+                    href="https://github.com/dhruvdhaduk-simform"
+                    imgURL="/logos/github.svg"
+                    label="GitHub"
+                    borderGradient={GRADIENTS[3].normal}
+                    borderGradientHover={GRADIENTS[3].hover}
+                />
+            </div>
         </div>
+    );
+}
+
+function ExternalLink({
+    href,
+    imgURL,
+    label,
+    borderGradient,
+    borderGradientHover,
+}: {
+    href: string;
+    imgURL: string;
+    label: string;
+    borderGradient: string;
+    borderGradientHover: string;
+}) {
+    return (
+        <a
+            href={href}
+            className={`p-0.5 bg-gradient-to-r ${borderGradient} rounded-lg min-w-40 md:min-w-48`}
+            target="_blank"
+        >
+            <span
+                className={`bg-black hover:bg-gradient-to-r ${borderGradientHover} rounded-lg flex justify-center items-center gap-2 text-nowrap p-2 px-6 md:p-3 md:px-8 text-sm md:text-base`}
+            >
+                <img className="w-4 md:w-5" src={imgURL} alt="" /> {label}
+            </span>
+        </a>
     );
 }
