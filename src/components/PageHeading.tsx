74aabeeab1a80b735993a557368b23db7c592b1f
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-05-16 10:44:49 +0530

    refractor: Add some comments in PageHeading component

--- src/components/PageHeading.tsx
+++ src/components/PageHeading.tsx
@@ -1,3 +1,4 @@
+/** Returns a heading with some styles for page heading */
 export function PageHeading({ title }: { title: string }) {
     return (
         <p role="heading" className="text-3xl md:text-4xl font-bold">
