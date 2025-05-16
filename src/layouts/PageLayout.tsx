3bb344d1af0dcd041bb49e29296a9a343f25cf03
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-05-16 10:49:23 +0530

    refractor: Add comments in PageLayout

--- src/layouts/PageLayout.tsx
+++ src/layouts/PageLayout.tsx
@@ -2,6 +2,7 @@ type PageLayout = {
     id?: string;
 } & React.PropsWithChildren;
 
+/** A wrapper which defines height, width, id for a Page component. */
 export function PageLayout({ id, children }: PageLayout) {
     return (
         <div id={id} className="w-full h-[100dvh]">
