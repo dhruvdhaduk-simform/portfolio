2cdcbe2ab01cc2db16c2289d75462822231fa9d3
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-05-16 10:56:33 +0530

    refractor: Create a seperate css module for marquee effect stylings

--- src/pages/SkillsPage.tsx
+++ src/pages/SkillsPage.tsx
@@ -1,12 +1,15 @@
 import { SKILLS } from '@/data/skills';
 import { PageHeading } from '@/components/PageHeading';
+import marqueeStyles from '@/styles/marquee.module.css';
 
 export function SkillsPage() {
     return (
         <div className="w-full h-full flex flex-col justify-center gap-8">
             <PageHeading title="Skills" />
 
-            <div className="w-full max-w-[50rem] overflow-hidden flex gap-5 marquee">
+            <div
+                className={`w-full max-w-[50rem] overflow-hidden flex gap-5 ${marqueeStyles.marquee}`}
+            >
                 <div className="flex shrink-0 gap-5 min-w-full">
                     {SKILLS.map((skill) => (
                         <a
