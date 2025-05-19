b3665a08145c165185ce06cd4083da0075002896
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-05-19 15:15:10 +0530

    refractor: Use 'PageLayoutProps' for props of PageLayout component

--- src/layouts/PageLayout.tsx
+++ src/layouts/PageLayout.tsx
@@ -1,9 +1,9 @@
-type PageLayout = {
+type PageLayoutProps = {
     id?: string;
 } & React.PropsWithChildren;
 
 /** A wrapper which defines height, width, id for a Page component. */
-export function PageLayout({ id, children }: PageLayout) {
+export function PageLayout({ id, children }: PageLayoutProps) {
     return (
         <div id={id} className="w-full h-[100dvh]">
             {children}
