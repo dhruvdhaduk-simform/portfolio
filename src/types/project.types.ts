f56c6e481594985d4e223c74269f3005861ec729
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-05-22 18:21:02 +0530

    refractor: Remove unnecessary comments from Project interface

--- src/types/project.types.ts
+++ src/types/project.types.ts
@@ -2,29 +2,15 @@
  * Interface representing a single Project.
  */
 export interface Project {
-    /**
-     * Title of the project.
-     */
     title: string;
-    /**
-     * Description of the project.
-     */
     description: string;
-    /**
-     * URL of the thumbnail of the project.
-     */
+    /** URL of the thumbnail of the project.  */
     thumbnail: string;
-    /**
-     * GitHub repo link of the project.
-     */
     githubLink: string;
     /**
      * Live Deployed link of the project.
      * Can be 'null' because the project might not be a website.
      */
     liveLink: string | null;
-    /**
-     * Tech stack used in the project.
-     */
     techStack: Array<string>;
 }
