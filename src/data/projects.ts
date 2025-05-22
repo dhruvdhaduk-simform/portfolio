ef3ce9901d930c01e6d8f7d78026073aada54708
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-05-22 18:25:44 +0530

    refractor: Move 'getRandomGradientImage' to its own file in 'utils/'

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,18 +1,5 @@
 import type { Project } from '@/types/project.types';
-
-/** Variable to rotate the gradient. */
-let currentIndex = 1;
-/**
- * Get the URL of a random gradient by rotating over a fixed set of gradients.
- * @returns the URL of a random gradient.
- */
-function getRandomGradientImage() {
-    const gradientURL = `/images/gradients_background/gradient${currentIndex}.png`;
-    currentIndex++;
-    if (currentIndex > 4) currentIndex = 1;
-
-    return gradientURL;
-}
+import { getRandomGradientImage } from '@/utils/getRandomGradientImage';
 
 /** Data for Projects section. */
 export const PROJECTS: Array<Project> = [
